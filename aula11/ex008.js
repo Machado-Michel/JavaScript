70762c22999467182b20c289948758f1a9664a1b
Michel Machado <126267099+Machado-Michel@users.noreply.github.com>
2023-05-30 13:34:14 -0300

    Criado ex008, 009 e 010

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.4
+console.log(`A velocidade do seu carro é de ${vel} Km/h.`)
+if (vel > 60) { // Condição Simples!
+    console.log(`Você Ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre utilizando o cinto de Segurança!`)
