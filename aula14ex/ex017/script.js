c07f5da73a09fdef583f8dc4445962236312a7e5
Michel Machado <126267099+Machado-Michel@users.noreply.github.com>
2023-06-14 20:49:21 -0300

    Criado ex017 da Aula 14

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,23 @@
-//alert('Olá!')
+function reiniciar(){
+    location.reload();
+}
+
+function tabuada(){
+let num = document.getElementById('txtn')    
+let tab = document.getElementById('seltab')
+if (num.value.length == 0 || num.value == ""){
+    alert('[ERRO] Digite um número!')
+    return;
+} else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = ''
+    while (c <= 10) {
+        let item = document.createElement('option')
+        item.text = `${n} X ${c} = ${n*c}`
+        item.value = `tab${c}`
+        tab.appendChild(item)
+        c++
+    }
+}
+}
